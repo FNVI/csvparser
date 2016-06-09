51c4f5b85395e9fcc85227f6b96c118051e943bd
Joe <joew@fnvi.co.uk>
2016-06-09 11:11:44 +0100

    Added everything required for unit tests

---
+++ tests/CSVMakerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of CSVMakerTest
+ *
+ * @author Joe Wheatley <joew@fnvi.co.uk>
+ */
+class CSVMakerTest {
+    //put your code here
+}
---
+++ tests/CSVParserTest.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of CSVReaderTest
+ *
+ * @author Joe Wheatley <joew@fnvi.co.uk>
+ */
+class CSVReaderTest {
+    //put your code here
+}
